5524117d1445718bb92a66deed988b596ba42cc8
Joel Jackson <DeepSky8@gmail.com>
2015-12-01 13:43:46 -0500

    Brief initial car characteristics, to confirm the paradigm.

---
+++ carPOS/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //This program needs to make available the car characteristics, the price for which it was purchased, and the date it was purchased
+        public void Car(Color, BodyStyle, TransmissionType, DateTime)
+        {
+
+        }
+
+
+
+        public enum Color { Red, Green, Blue, Black, Tan, Gold, Silver, White};
+        public enum BodyStyle { Coupe, Sedan, Pickup, Van };
+        public enum TransmissionType { Automatic, Manual };
+    }
+}
