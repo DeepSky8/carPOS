49bf44e7da03e4ae64ea941c71d27ea9bc5a99b3
Joel Jackson <DeepSky8@gmail.com>
2015-12-01 14:32:57 -0500

    Actually figured out what the class looks like, rather than Car Method. Created console write method within car class.

--- carPOS/ConsoleApplication1/Program.cs
+++ carPOS/ConsoleApplication1/Program.cs
@@ -12,16 +12,30 @@ namespace ConsoleApplication1
         {
         }
 
-        //This program needs to make available the car characteristics, the price for which it was purchased, and the date it was purchased
-        public void Car(Color, BodyStyle, TransmissionType, DateTime)
-        {
-
-        }
-
-
-
-        public enum Color { Red, Green, Blue, Black, Tan, Gold, Silver, White};
+        public enum Color { Red, Green, Blue, Black, Tan, Gold, Silver, White };
         public enum BodyStyle { Coupe, Sedan, Pickup, Van };
         public enum TransmissionType { Automatic, Manual };
+        public enum CarMaker { Ford, Chrysler, Chevrolet, Honda, Saturn, Dodge }
+
+        //This program needs to make available the car characteristics, the price for which it was purchased, and the date it was purchased
+        public class Car {
+            public Color color; 
+            public BodyStyle bodyStyle; 
+            public TransmissionType transmissionType; 
+            public CarMaker carMaker; 
+            public DateTime yearMade;
+            
+            public void printCar()
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", color, bodyStyle, transmissionType, carMaker, yearMade );
+            } 
+
+
+}
+
+
+
+
+
     }
 }
